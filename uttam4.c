1006ed27891fd869d5b1149d64d17b8b09b736ca
Rajnandanikumari <118913721+Rajnandanikumari@users.noreply.github.com>
2023-01-25 22:53:33 +0530

    Add files via upload

---
+++ uttam4.c
@@ -0,0 +1,94 @@
+#inlude<stdio.h>
+int main()
+{
+    int i;
+    for(i=1;i<=5;i++)
+        printf("iNeuron\n");
+    return 0;
+}
+
+#include<stdio.h>
+int main()
+{
+    int i;
+    for(i=1;i<=10;i++)
+   printf("%d",i);
+   return 0;
+}
+
+#include<stdio.h>
+int main()
+{
+    int i;
+    for(i=10;i>=1;i--)
+        printf("%d\n",i);
+    return 0;
+}
+
+#include<stdio.h>
+int main()
+{
+    int num;
+    printf("Enter the number\n");
+scanf("%d\n",&num);
+for(i=1;i<=10;i++)
+    num=2*num-1;
+printf("%d\n",num)
+return 0;
+}
+
+#include<stdio.h>
+int main()
+{
+    int num;
+    printf("Enter the number\n");
+    scanf("%d\n",&num);
+    for(i=10;i<=1;i--)
+        num=2*num-1;
+    printf("%d\n",num);
+    return 0;
+}
+
+#include<stdio.h>
+int main()
+{
+    printf("Enter the number\n");
+    scanf("%d",&num);
+    for(i=1;i<=10;i++)
+        printf("%d*%d=%d\n"n,i,n*i);
+}
+return 0;
+}
+
+#include<stdio.h>
+int main()
+{
+    int i,n,s=1;
+    printf("Enter a number\n");
+    scanf("%d\n",&n);
+    for(i=0;i<=10;i++)
+    {
+        s=(n*(n+1)*(2*n+1))/6;
+    }
+    printf("\n square of %d is %d",n,s);
+    return 0;
+}
+
+#include<stdio.h>
+int main()
+{
+    int n=5;
+    printf("Enter the number\n");
+    scanf("%d\n",&n);
+     for(i=1;i<=10;i++)
+     {
+         printf("%d*%d=%d\n",n,i,i*n);
+     }
+     return 0;
+     }
+
+
+
+
+
+

81f3a261d3eda7a018a34e1b2c8d19a7e936a4ef
Rajnandanikumari <118913721+Rajnandanikumari@users.noreply.github.com>
2023-01-25 14:40:05 +0530

    Add files via upload

---
+++ uttam1.c
@@ -0,0 +1,82 @@
+#include<stdio.h>
+int main()
+{
+    printf("Hello");
+}
+
+#include<stdio.h>
+int main()
+{
+    printf("Hello\n");
+    printf("students");
+}
+
+#include<stdio.h>
+int main()
+{
+    printf("\"Hello\"")
+}
+
+#include<stdio.h>
+int main()
+{
+    int r,a;
+    printf("Enter the radius\n");
+    scanf("%d",r);
+    a=3.14*r*r
+    printf("%f",a);
+}
+#include<stdio.h>
+int main()
+{
+    char name[]={'p','r','t','e','k','\0'}
+    int i,length=0;
+    printf("%s",name);
+    for(i=0;i<=10;i++)
+    {
+        length++;
+
+    }
+ printf("length of string is %d\n",strien(name));
+ return 0;
+}
+
+#include<stdio.h>
+int main()
+{
+   printf("%d\n");
+}
+
+#include<stdio.h>
+int main()
+{
+    printf("\n");
+}
+#include<stdio.h>
+int main()
+{
+    printf("\\");
+}
+
+#include<stdio.h>
+int main()
+{
+  int date,month,year;
+  printf("Enter the date,month and year\n");
+  scanf("%d=month,%d=month,%d=year");
+  return 0;
+}
+
+#include<stdio.h>
+int main()
+{
+ int hour,minute;
+ printf("Enter time in HH:MM\n");
+ scanf("%d%d",&hour&minute);
+ printf("hour=%d,minute=%d\n",hour,min);
+ return 0;
+}
+
+output
+ineuron 7
+

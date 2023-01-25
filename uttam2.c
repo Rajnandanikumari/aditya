d96712ec91f5fdbe9e37d63683e2367180a3b5fd
Rajnandanikumari <118913721+Rajnandanikumari@users.noreply.github.com>
2023-01-25 15:55:22 +0530

    Add files via upload

---
+++ uttam2.c
@@ -0,0 +1,142 @@
+#include<stdio.h>
+int main()
+{
+    int num;
+    printf("Enter the number\n);
+    scanf("%d",&num);
+    num=num%10
+    printf("%d",num);
+    return 0;
+}
+
+#include<stdio.h>
+int main()
+{
+    int num;
+    printf("Enter the number\n");
+    scanf("%d",&num);
+    num=num/10;
+    printf("%d",num);
+    return 0;
+}
+
+#include<stdio.h>
+int main()
+{
+    int a,b,t;
+    printf("Enter the number\n");
+    scanf("%d%d",&a&b);
+    t=a;
+    a=b;
+    b=t;
+    printf("a=%d,b=%d",a,b);
+    return 0;
+}
+
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("Enter the number\n");
+    scanf("%d%d",&a,&b);
+    a=a+d;
+    b=a-b;
+    a=a-b;
+    printf("a=%d,b=%d",a,b);
+    return 0;
+}
+
+#include<stdio.h>
+int main()
+{
+    int x;
+    printf("Enter the number\n");
+    scanf("%d",&x);
+    x=x%10
+    x=x+x%10;
+    printf("%d",x);
+    return 0;
+}
+
+#include<stdio.h>
+int main()
+{
+    char name;
+    printf("Enter the name\n");
+    scanf("%d",&num);
+    printf("ASCII code of %c is %d",ch,ch);
+    return 0;
+}
+
+#include<stdio.h>
+int main()
+{
+    int num;
+    printf("Enter the number\n");
+    scanf("%d",&num);
+    if(num&1)
+        printf("Number is odd\n");
+    else
+        printf("Number is even\n");
+    return 0;
+}
+
+#include<stdio.h>
+int main()
+{
+    printf("size of int is %d bytes\n",sizeof(int));
+        printf("size of float is %d bytes\n",sizeof(float));
+            printf("size of char is %d bytes\n",sizeof(char));
+           printf("size of double is %d bytes\n",sizeof(double));
+           return 0;
+}
+
+#include<stdio.h>
+int main()
+{
+    int x=4565;
+    y=x%10;
+    x=x-y;
+    printf("%d",x);
+    return 0;
+}
+
+#include<stdio.h>
+int main()
+{
+    int x=234,y=9;
+    x=x*10;
+    x=x+9;
+    printf("%d",x);
+    return 0;
+}
+
+#include<stdio.h>
+int main()
+{
+    float INR=100,USD=0;
+    USD=INR/76.23;
+    printf("INR value=%.2f and USD value=%.2f",INR,USD);
+    return 0;
+}
+
+#include<stdio.h>
+int main()
+{
+  int num,r;
+  printf("Enter the number\n");
+  scanf("%d",&num);
+  r=num%10;
+  temp=x/10;
+  num=(r*100)+temp;
+  printf("%d",num);
+  return 0;
+}
+
+
+
+
+
+
+
+}

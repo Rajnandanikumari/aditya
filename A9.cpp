79c9af4aafc429daa817153b6750bb1faaab4ff6
Codeco <118913721+Rajnandanikumari@users.noreply.github.com>
2023-10-22 02:19:18 +0530

    Add files via upload

---
+++ A9.cpp
@@ -0,0 +1,15 @@
+//print linear search //
+
+#include<iostream>
+#include<stdio.h>
+int main()
+{
+  using namespace std;
+  int arr[]={4,2,5,6,7};
+   for(int i=0;i<6;i++)
+// if(arr[i]>arr[i]+1)
+ {
+    cout<<arr[i]<<"\n";
+ }
+  return 0;
+}

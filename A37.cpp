5ac026587e40e1482a7ec870aab582bf39844424
Codeco <118913721+Rajnandanikumari@users.noreply.github.com>
2023-10-29 11:55:25 +0530

    Add files via upload

---
+++ A37.cpp
@@ -0,0 +1,24 @@
+// Intersection of array //
+
+#include<iostream>
+#include<stdio.h>
+int main()
+{
+    using namespace std;
+  //  int n=sizeof(arr)/sizeof(int);
+    int n=6;
+    int sum=0;
+    int a[5]={1,4,5,6,7};
+    int b[5]={6,7,8,9,2};
+    {
+    for(int i=0;i<6;i++)
+    {
+     for(int j=i;j<6;j++)
+    {
+      if(a[i]==b[j])
+    }
+    }
+    cout<<a[i];
+    }
+    return 0;
+}

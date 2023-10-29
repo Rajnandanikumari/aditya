e32ad18fb362ca6652549351a4e08b2d96908529
Codeco <118913721+Rajnandanikumari@users.noreply.github.com>
2023-10-29 11:48:20 +0530

    Add files via upload

---
+++ A26.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<iostream>
+#include<stdio.h>
+int main()
+{
+    using namespace std;
+    int arr[]={3,4,5,2,3,4};
+    int n=sizeof(arr)/sizeof(int);
+    int count=0;
+     for(int i=0; i<n;i++){
+         count =0;
+          for(int j=0; j<n;j++)
+           {
+            if(arr[i]==arr[j])
+            {
+             count++;
+             cout<<"Repeation of number is : "<<arr[i]<<"is"<<count<<"\n";
+      }
+           }
+        else if{
+          cout<<"Single number is : "<<arr[i]<<"is"<<count<<"\n";
+      }
+     }
+    return 0;
+}

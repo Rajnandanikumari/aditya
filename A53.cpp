6970900ec92dd4edef18322d7f04a3ef021ef79f
Codeco <118913721+Rajnandanikumari@users.noreply.github.com>
2023-11-03 11:09:08 +0530

    Add files via upload

---
+++ A53.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+int main()
+{
+    int rows,cols;
+    int arr[3][3];
+    int sum=0;
+    cout<<"Enter the rows\n";
+    cin>>rows;
+    cout<<"Enter the cols\n";
+    cin>>cols;
+    for(int i=0;i<3;i++)
+    {
+        for(int j=0;j<3;j++)
+        {
+            cin>>arr[i][j];
+        }
+    }
+    for(int i=0;i<3;i++)
+    {
+        for(int j=0;j<3;j++)
+        {
+            sum=sum+arr[i][j];
+        }
+        cout<<"Sum of number\n"<<sum<<"\n";
+    }
+    return 0;
+        }

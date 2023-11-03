7d8c164d73f07e3fa5707b3ce8856ce9d27c2809
Codeco <118913721+Rajnandanikumari@users.noreply.github.com>
2023-11-03 11:16:15 +0530

    Add files via upload

---
+++ A58.cpp
@@ -0,0 +1,37 @@
+
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+int kthmin()
+{
+    int arr[2][2]={{2,3},{4,5}};
+    int min=arr[0][0];
+    for(int i=0;i<2;i++)
+    {
+        for(int j=0;j<2;j++)
+        {
+        if(arr[i][j]<min)
+        min=arr[i][j];
+    {
+        cout<<min<<"\n";
+        break;
+    }
+     if else 
+    {
+        cout<<max<<"\n";
+    }
+      break;
+   }
+
+   return 0;
+}
+
+
+int main()
+{
+    using namespace std;
+  //  int arr[]={2,3,4,5};
+    cout<<kthmin();
+  // cout<< kthmax();
+    return 0;
+}
